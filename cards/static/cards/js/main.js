3238effabf5bfc886ace9ff72835dba81ab3386e
UkElena2023 <ukolova_elena@mail.ru>
2024-05-28 14:56:18 +0300

    Django_PY331: добавила папку static в приложение cards

---
+++ cards/static/cards/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('h1').addEventListener('click', () => {
+    document.querySelector('h1').classList.toggle('text-danger');
+    document.querySelector('h1').classList.toggle('bg-warning');
+});
